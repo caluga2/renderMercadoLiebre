3612d3ebf18b3fff49cab2ec21235d818f5e7476
caluga2 <carolfreitas50@gmail.com>
2022-10-25 22:34:57 -0300

    cambio puerto

--- app.js
+++ app.js
@@ -3,10 +3,10 @@ const express = require("express");
 const app = express();
 
 app.use(express.static(__dirname+ "/public"))
-    
-    app.listen(process.env.PORT || 3000, () => {
-        console.log("Servidor funcionando");
-    });
+
+const port = process.env.PORT || 3001;
+
+app.listen(port, () => console.log('Servidor corriendo en el puerto ${port}'));
 
 app.get('/',(req,res)=>{
     res.sendFile(__dirname+'/views/home.html')
